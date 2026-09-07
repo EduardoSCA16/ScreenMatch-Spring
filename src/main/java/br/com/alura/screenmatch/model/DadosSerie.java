f77ae8af7ce254ce5cebe374c3ff924df67274e5
EduardoSCA16 <eduardosca16@gmail.com>
2026-09-07 18:23:02 -0300

    refactor: convert DadosSerie to record

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,4 +1,10 @@
 package br.com.alura.screenmatch.model;
 
-public class DadosSerie {
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+// @JsonAlias é uma anotação do Jackson usada para apelidar
+// @JsonProperty é uma anotação do Jackson que define o nome exato
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
