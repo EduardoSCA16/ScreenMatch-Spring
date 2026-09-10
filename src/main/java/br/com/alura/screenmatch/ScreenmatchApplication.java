9ff8ddf80bb2fd78d82ade081381e51593e66f31
EduardoSCA16 <eduardosca16@gmail.com>
2026-09-09 22:15:31 -0300

    feat dados episodio e temporada

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,14 +22,26 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
+		ConverteDados conversor = new ConverteDados();
 
 		var json = consumoApi.obterDadosApi("https://www.omdbapi.com/?t=the+mentalist&apikey=6585022c");
-		System.out.println("Série:\n" + json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println("Coffee do dia:\n" + json);
+		System.out.println("JSON da Série: " + json);
 
-		ConverteDados conversor = new ConverteDados();
 		DadosSerie dadosSerie = conversor.converterDados(json, DadosSerie.class);
 		System.out.println(dadosSerie);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		// Mostrando todas as temporadas
+		for (int i = 1; i <= dadosSerie.TotalTemporadas(); i++) {
+			json = consumoApi.obterDadosApi("https://www.omdbapi.com/?t=the+mentalist&season=" + i + "&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.converterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
+		json = consumoApi.obterDadosApi("https://www.omdbapi.com/?t=the+mentalist&season=1&episode=1&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.converterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
--- src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -1,7 +1,5 @@
 package br.com.alura.screenmatch.service;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-
 public interface IConverteDados {
     <T> T converterDados(String json, Class<T> classe);
 }
